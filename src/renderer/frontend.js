24baf110fe9855e4d0ae26c49c4ceb27883eb919
TeChRF24 <f.rafael.temich24@gmail.com>
2023-02-20 00:19:29 -0600

    Changing image to display

--- src/renderer/frontend.js
+++ src/renderer/frontend.js
@@ -1,7 +1,22 @@
-// import os from 'os'
 const os = require('os')
 window.addEventListener('load', () => {
     // document.getElementById('mensaje').innerHTML = 'Message inserted by JS'
-    console.log(os.cpus())
-    console.log('diferente')
-})
+    // console.log(os.cpus())
+    addImagesEvents();
+});
+
+function addImagesEvents() {
+    const thumbs = document.querySelectorAll('li.list-group-item');
+
+    for (let i = 0, length1 = thumbs.length; i < length1; i++) {
+        thumbs[i].addEventListener('click', function() {
+            changeImage(this);
+        })
+    }
+}
+
+function changeImage(node) {
+    document.querySelector('li.selected').classList.remove('selected');
+    node.classList.add('selected');
+    document.getElementById('image-displayed').src = node.querySelector('img').src;
+}
